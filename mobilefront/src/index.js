4d873c100e688b8c6ae553fe74afb3fecf393bdc
Eliot <gaillegue.eliot@gmail.com>
2023-01-03 16:02:41 +0100

    added mobile view skeleton

---
+++ mobilefront/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
+import Navbar from "./components/navbar/navbar"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
