4bfd2e4ad876e26943d5a9e5614dfc8ec9513a8b
Mouhamad Al Moustapha Mbaye <moustapha-mbaye@outlook.com>
2023-01-03 16:46:43 +0100

    Merge from main (#3)
    * added mobile view skeleton
    
    * fin de l'apiwebshop
    
    Co-authored-by: Eliot <gaillegue.eliot@gmail.com>
    Co-authored-by: EliotG <61939917+Lezgig@users.noreply.github.com>
    Co-authored-by: Ikon-design <77398364+Ikon-design@users.noreply.github.com>

--- APIWebshop/app.js
+++ APIWebshop/app.js
@@ -42,6 +42,56 @@ app.get('/orders', async (req, res) => {
   }
 });
 
+app.get('/customers/:id', async (req, res) => {
+  try {
+    const response = await fetch(`${baseUri}/customers/${req.params.id}`);
+    const customer = await response.json();
+    res.json(customer);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/customers/:id/orders', async (req, res) => {
+  try {
+    const response = await fetch(`${baseUri}/customers/${req.params.id}/orders`);
+    const orders = await response.json();
+    res.json(orders);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/customers/:id/orders/:id', async (req, res) => {
+  try {
+    const response = await fetch(`${baseUri}/customers/${req.params.id}/orders/${req.params.id}`);
+    const order = await response.json();
+    res.json(order);
+  } catch (error) {
+    res.status(500).send
+  }
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const response = await fetch(`${baseUri}/products`);
+    const products = await response.json();
+    res.json(products);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const response = await fetch(`${baseUri}/products/${req.params.id}`);
+    const product = await response.json();
+    res.json(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 app.listen(3000, () => {
   console.log('API listening on port 3000');
 });
