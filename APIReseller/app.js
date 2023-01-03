0b5c6884f0b932c2478bb6f0777ab5ceefb3ea96
Mouhamad Al Moustapha Mbaye <moustapha-mbaye@outlook.com>
2023-01-03 16:24:11 +0100

    Reseller API

---
+++ APIReseller/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import fetch from 'node-fetch';
+const app = express();
+
+
+const baseUri = 'https://615f5fb4f7254d0017068109.mockapi.io/api/v1';
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l\'API de PayeTonKawa !');
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const response = await fetch(`${baseUri}/customers`);
+    const customers = await response.json();
+    console.log(customers);
+    res.json(customers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('API listening on port 3000');
+});
