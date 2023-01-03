07ac41400f44e77d2f2891e6a647e66a15a83189
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2023-01-03 15:48:35 +0100

    premier get

--- APIWebshop/app.js
+++ APIWebshop/app.js
@@ -1,8 +1,23 @@
-const express = require('express');
+import express from 'express';
+import fetch from 'node-fetch';
 const app = express();
 
+
+const baseUri = 'https://615f5fb4f7254d0017068109.mockapi.io/api/v1';
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Bienvenue sur l\'API de PayeTonKawa !');
+});
+
+app.get('/customers', async (req, res) => {
+  try {
+    const response = await fetch(`${baseUri}/customers`);
+    const customers = await response.json();
+    console.log(customers);
+    res.json(customers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(3000, () => {
