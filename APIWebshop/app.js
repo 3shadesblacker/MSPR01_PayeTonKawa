e9198bbe6e0462b6e5cc49244f1dcbe1463b8214
Clément Legrix <77398364+Ikon-design@users.noreply.github.com>
2023-01-04 19:01:33 +0100

    truc qui marche pas

--- APIWebshop/app.js
+++ APIWebshop/app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
 import jsonwebtoken from 'jsonwebtoken';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
 import * as dotenv from 'dotenv'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 dotenv.config();
 
 const baseUri = 'https://615f5fb4f7254d0017068109.mockapi.io/api/v1';
