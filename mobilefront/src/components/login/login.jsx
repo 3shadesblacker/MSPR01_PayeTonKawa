168f5ea7f848a9e20c42353bcd73001bb15f8818
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2023-01-13 23:17:44 +0100

    test front

--- mobilefront/src/components/login/login.jsx
+++ mobilefront/src/components/login/login.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import './login.css';
 
 const Login = () => {
-    debugger
+
     return (
-        <div>
-            <h1>Login :</h1>
-        </div>
+        <>
+            <h1>Paye Ton Kawa</h1>
+            <div className='loginForm'>
+                <h2>Identification</h2>
+                <p>Afin de vous identifier sur l’application merci de renseigner votre adresse mail.</p>
+                <input></input>
+                <button>Envoyer mail    </button>
+            </div>
+        </>
     )
 }
 
