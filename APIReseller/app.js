19beab8fcbc3eb09c6aa289781518c39d1892b51
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2023-01-18 17:09:45 +0100

    Changement le port exposant

--- APIReseller/app.js
+++ APIReseller/app.js
@@ -101,6 +101,6 @@ app.post("/submit", (req, res) => {
   });
 })
 
-app.listen(3000, () => {
-  console.log('API listening on port 3000');
+app.listen(3001, () => {
+  console.log('API listening on port 3001');
 });
