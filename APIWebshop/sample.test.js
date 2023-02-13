d8cf262508de24dead24167e2ee6eb44886ca6f4
amadou <keitaamadou3172@gmail.com>
2023-02-13 10:34:14 +0100

    premiere fonction teste en jest

--- APIWebshop/sample.test.js
+++ APIWebshop/sample.test.js
@@ -3,3 +3,15 @@ describe('Sample Test', () => {
       expect(true).toBe(true)
     })
   })
+  const fetch = require('node-fetch');
+const baseUri = 'http://localhost:3000';
+
+test('Get customer by ID', async () => {
+  const id = 1;
+  const response = await fetch(`${baseUri}/customers/${id}`);
+  const customer = await response.json();
+  expect(customer.id).toBe(id);
+});
+
+
+
