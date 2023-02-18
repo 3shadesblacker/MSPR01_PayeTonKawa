03fc272e2d43a6c7f6731180d6891e6ff10ad437
ypsen <gaillegue.eliot@gmail.com>
2023-02-18 17:21:34 +0000

    adding qrCode Scanner logic

--- appMobile/App.js
+++ appMobile/App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 import React from "react";
 import { NativeRouter, Route, Link, Routes, Outlet } from "react-router-native";
-import Cafe from "./components/coffee/displayCoffee";
+// import Cafe from "./components/coffee/displayCoffee";
+import Scanner from './components/qrCode/scanner';
 
 export default function App() {
   return (
     <NativeRouter>
        <Routes>
-        <Route exact path="/" element={<Cafe />} />
+        <Route exact path="/" element={<Scanner />} />
       </Routes>
     </NativeRouter>
   );
