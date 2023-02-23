a8d1f950c9ec8576faa9f08d7bf3780e8b4907a9
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2023-02-23 07:49:08 +0100

    Changement api pour utilisation dolibarr

--- APIWebshop/app.js
+++ APIWebshop/app.js
@@ -6,87 +6,72 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json' assert { type: "json" };
 import * as dotenv from 'dotenv'
 import cryptoJs from 'crypto-js'
+import http from 'http'
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 dotenv.config();
 
-const baseUri = 'https://615f5fb4f7254d0017068109.mockapi.io/api/v1';
+const baseUri = process.env.BASE_URI;
 
-app.post("/login", (req, res) => {
-  const { identifiant, password } = req.body;
-  if (identifiant === process.env.IDENTIFIANT && password === process.env.PASSWORD) {
-    const token = generateAccessToken(identifiant + password);
-    res.send(encrypt(token));
-  } else {
-    res.status(403).send("Identifiant ou mot de passe incorrect");
-  }
-})
-
-app.get('/',  (req, res) => {
+app.get('/', (req, res) => {
   res.send('Bienvenue sur l\'API de PayeTonKawa !');
 });
 
-app.get('/customers', authentification, async (req, res) => {
-  try {
-    const response = await fetch(`${baseUri}/customers`);
-    const customer = await response.json();
-    res.json(customer);
-  } catch (error) {
-    res.status(500).send(error);
+app.post("/login", async (req, res) => {
+  const { identifiant, password } = req.body;
+  if (identifiant === process.env.IDENTIFIANT && password === process.env.PASSWORD) {
+    await fetch(`${baseUri}/login?login=${identifiant}&password=${password}&reset=1`, generateHeader("GET"))
+      .then(response => response.text())
+      .then(result => {
+        const jsonResponse = JSON.parse(result)
+        const sendValue = {
+          DOLAPIKEY: encrypt(jsonResponse.success.token),
+          token: encrypt(generateAccessToken(jsonResponse.success.token))
+        }
+        res.status(200).send(JSON.stringify(sendValue));
+      })
+      .catch(error => {
+        console.log(error),
+          res.status(403).send(error)
+      });
+  } else {
+    console.log("Identifiant ou mot de passe incorrect")
+    res.status(403).send("Identifiant ou mot de passe incorrect");
   }
 });
-
-app.get('/customers/:id', authentification, async (req, res) => {
-  try {
-    const response = await fetch(`${baseUri}/customers/${req.params.id}`);
-    const customer = await response.json();
-    res.json(customer);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get('/customers/:id/orders', authentification, async (req, res) => {
-  try {
-    const response = await fetch(`${baseUri}/customers/${req.params.id}/orders`);
-    const orders = await response.json();
-    res.json(orders);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get('/customers/:id/orders/:id', authentification, async (req, res) => {
-  try {
-    const response = await fetch(`${baseUri}/customers/${req.params.id}/orders/${req.params.id}`);
-    const order = await response.json();
-    res.json(order);
-  } catch (error) {
-    res.status(500).send
-  }
-});
-
+// 
 app.get('/products', authentification, async (req, res) => {
-  try {
-    const response = await fetch(`${baseUri}/products`);
-    const products = await response.json();
-    res.json(products);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+  const { DOLAPIKEY } = req.body;
+  await request(`${baseUri}/products`, "GET", DOLAPIKEY, res);
 });
-
+// 
 app.get('/products/:id', authentification, async (req, res) => {
-  try {
-    const response = await fetch(`${baseUri}/products/${req.params.id}`);
-    const product = await response.json();
-    res.json(product);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+  const { DOLAPIKEY } = req.body;
+  await request(`${baseUri}/products/${req.params.id}`, "GET", DOLAPIKEY, res);
+});
+// 
+app.get('/customers', authentification, async (req, res) => {
+  const { DOLAPIKEY } = req.body;
+  await request(`${baseUri}/thirdparties?mode=1`, "GET", DOLAPIKEY, res);
+});
+// 
+app.get('/orders', authentification, async (req, res) => {
+  const { DOLAPIKEY } = req.body;
+  await request(`${baseUri}/orders`, "GET", DOLAPIKEY, res);
+});
+// 
+app.get('/prospect', authentification, async (req, res) => {
+  const { DOLAPIKEY } = req.body;
+  await request(`${baseUri}/thirdparties?mode=2`, "GET", DOLAPIKEY, res);
+})
+// 
+app.get('/stocks', authentification, async (req, res) => {
+  const { DOLAPIKEY } = req.body;
+  await request(`${baseUri}/stockmovements`, "GET", DOLAPIKEY, res);
 });
 
 app.listen(3000, () => {
@@ -99,9 +84,9 @@ function generateAccessToken(value) {
 }
 
 function authentification(req, res, next) {
-  if(req.headers.authorization == null) return res.sendStatus(401);
-  const token = decrypt(req.headers.authorization)
-  if (token == null) return res.sendStatus(401);
+  if (req.headers.authorization == null) return res.sendStatus(401);
+  const token = decrypt(req.headers.authorization);
+  console.log(token)
   jsonwebtoken.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
@@ -109,10 +94,43 @@ function authentification(req, res, next) {
   });
 }
 
-function decrypt(value){
+function decrypt(value) {
   return cryptoJs.AES.decrypt(value, process.env.SALT).toString(cryptoJs.enc.Utf8);
 }
 
-function encrypt(value){
+function encrypt(value) {
   return cryptoJs.AES.encrypt(value, process.env.SALT).toString();
+}
+
+function generateHeader(Methode, DOLAPIKEY) {
+  console.log(Methode, DOLAPIKEY)
+  if (DOLAPIKEY) {
+    return {
+      method: Methode,
+      headers: {
+        DOLAPIKEY: DOLAPIKEY,
+        Accept: "application/json"
+      }
+    };
+  }
+  return {
+    method: Methode
+  };
+}
+
+async function request(uri, method, DOLAPIKEY, res) {
+  if (DOLAPIKEY !== undefined) {
+    await fetch(uri, generateHeader(method, decrypt(DOLAPIKEY)))
+      .then(response => response.text())
+      .then(result => {
+        res.status(200).send(JSON.parse(result));
+      })
+      .catch(error => {
+        res.status(403).send(error)
+      })
+  }
+  else {
+    res.status(403).send("Vous n'êtes pas autorisé à accéder à cette ressource")
+
+  }
 }
